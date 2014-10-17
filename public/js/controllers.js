bebc8f5d1fd09ef64acc2be95dc5aa36e55564ac
Axel Örn Sigurðsson <axel@absalon.is>
2014-10-17 13:18:36 +0000

    Add AngularJS for the fronend part
    'hello world' added.

---
+++ public/js/controllers.js
@@ -0,0 +1,3 @@
+angular.module('vnstatApp').controller('mainCtrl', ['$scope', function ($scope) {
+    $scope.world = 'world';
+}]);
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var vnstat = require('vnstat-dumpdb'),
     cacheManager = require('cache-manager'),
     util = require('util'),
-    express = require('express');
+    express = require('express'),
+    path = require('path');
 
 var app = express();
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.use('/angular', express.static(path.join(__dirname, 'node_modules/angular')));
+app.use(express.static(path.join(__dirname, 'public')));
+
 var cache = cacheManager.caching({
     store: 'memory',
     max: 100,
@@ -22,7 +28,7 @@ function getVnstat(callback) {
 	});
 }
 
-app.get('/', function (req, res) {
+app.get('/vnstat/', function (req, res) {
     var callback = function callback(data) {
         res.type('text/plain');
         res.send(data);
@@ -43,6 +49,12 @@ app.get('/', function (req, res) {
     });
 });
 
-var port = process.env.PORT || 8080;
-console.log("Listening on port " + port);
-app.listen(port);
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {
+        root: path.resolve(app.get('views'))
+    });
+});
+
+app.listen(app.get('port'), function () {
+    console.log("Listening on port %d", app.get('port'));
+});
